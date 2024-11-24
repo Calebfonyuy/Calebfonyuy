0e44f8cf88b22fd7ee78132157fea873662b2c67
Calebfonyuy <fonyuyca@gmail.com>
2024-11-24 14:12:58 +0100

    added professional experiences

--- src/components/sections/ProfessionalExperience.vue
+++ src/components/sections/ProfessionalExperience.vue
@@ -7,6 +7,11 @@
                 <span class="list-element list-institution">{{ experience.company }}</span>
                 <span class="list-element list-date">{{ experience.duration }}</span>
                 <span class="list-element list-detail">{{ experience.description }}</span>
+                <ul class="mt-2">
+                    <li class="list-element list-detail" v-for="detail in experience.details" :key="detail">
+                        <b>{{ detail.title }}</b>: {{ detail.info }}
+                    </li>
+                </ul>
             </li>
         </ul>
     </div>
@@ -19,11 +24,122 @@ export default {
         return {
             experiences: [
                 {
+                    id: 6,
+                    title: 'Doctoral Researcher',
+                    company: 'INRIA Rennes, France',
+                    duration: 'February 2024 - Present, Full-time',
+                    description: 'Researching on Live Migration of Virtual Machines on Heterogeneous Processors.',
+                    details: [
+                        {
+                            title: 'Directors',
+                            info: 'Prof. Alain Tchana and Prof. David Bromberg.'
+                        },
+                        {
+                            title: 'Co-advisors',
+                            info: 'Dr. Djob Mvondo and Dr. Renaud Lachaize'
+                        },
+                        {
+                            title: 'Description',
+                            info: 'Researching on the live migration of virtual machines on heterogeneous processors.'
+                        }
+                    ]
+                },
+                {
+                    id: 5,
+                    title: 'Full Stack Developer',
+                    company: 'Yadiko, Yaounde, Cameroon',
+                    duration: 'September 2023 - April 2024, Part-time',
+                    description: 'Developing and maintaining the Yadiko school management platform.',
+                    details: [
+                        {
+                            title: 'Technologies',
+                            info: 'PHP, Laravel, MySQL'
+                        },
+                        {
+                            title: 'Technical Tasks',
+                            info: 'Developing new features, fixing bugs, and maintaining the platform.'
+                        }
+                    ]
+                },
+                {
+                    id: 4,
+                    title: 'Technical Lead',
+                    company: 'IT Solutions and Advisory, Yaounde, Cameroon',
+                    duration: 'April 2023 - September 2023, Full-time',
+                    description: 'Leading a team of developers to deliver high-quality software solutions.',
+                    details: [
+                        {
+                            title: 'Technologies',
+                            info: 'PHP, Laravel, MySQL, bash, Git'
+                        },
+                        {
+                            title: 'Technical Tasks',
+                            info: 'Project management, Deployment, System administration, Backend development'
+                        },
+                        {
+                            title: 'Management Tasks',
+                            info: 'Team management, Project management, Client communication'
+                        }
+                    ]
+                },
+                {
+                    id: 3,
+                    title: 'Research Engineer',
+                    company: 'OFTY Cameroun, Yaounde, Cameroon',
+                    duration: 'November 2021 - December 2022, Full-time',
+                    description: 'Study of Live migration of Virtual Machines on Heterogeneous Processors.',
+                    details: [
+                        {
+                            title: 'Technologies',
+                            info: 'Xen, Linux, Unikraft, C, C++, Python'
+                        },
+                        {
+                            title: 'Supervisors',
+                            info: 'Prof. Alain Tchana and Dr. Djob Mvondo'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    title: 'Research Intern',
+                    company: 'Laboratoire Informatique du Parallelisme(LIP), Lyon, France',
+                    duration: 'March 2021 - September 2021, Full-time',
+                    description: 'Conception and Proof of an Open Byzantine and Sybil Tolerant Group Membership Protocol',
+                    details: [
+                        {
+                            title: "Domain",
+                            info: "Distributed Systems and Blockchain"
+                        },
+                        {
+                            title: 'Technologies',
+                            info: 'Rust, Python, Grid`5000'
+                        },
+                        {
+                            title: 'Supervisors',
+                            info: 'Prof. Alain Tchana and Prof Vivien Quema'
+                        }
+                    ]
+                },
+                {
                     id: 1,
-                    title: 'Software Engineer',
-                    company: 'Tech Company',
-                    duration: 'Jan 2020 - Present',
-                    description: 'Developing and maintaining web applications.'
+                    title: 'Full Stack Developer',
+                    company: 'Eden Engineering, Yaounde, Cameroon',
+                    duration: 'June 2019 - October 2021, Part-time',
+                    description: 'Developing and maintaining SIGELAM, a medical laboratory management platform.',
+                    details: [
+                        {
+                            title: 'Technologies',
+                            info: 'PHP, Laravel, MySQL, bash, Git, Python'
+                        },
+                        {
+                            title: 'Technical Tasks',
+                            info: 'System design and conception, Fullstack development, Deployment, System administration, Database management'
+                        },
+                        {
+                            title: 'Management Tasks',
+                            info: 'Client communication, Client Training, Requirement gathering, Project management'
+                        }
+                    ]
                 }
             ]
         };
