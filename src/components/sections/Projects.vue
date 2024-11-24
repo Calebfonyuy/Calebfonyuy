876d593053e451aa9ac0b256ae4cc5808f3420ae
Calebfonyuy <fonyuyca@gmail.com>
2024-11-24 14:31:46 +0100

    Added projects

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -3,10 +3,13 @@
         <span class="page-title">Projects</span>
         <div v-for="project in projects" :key="project.id" class="project">
             <span class="list-element list-title">{{ project.name }}</span>
-            <span class="list-element list-detail">{{ project.description }}</span>
             <span class="list-element list-date">{{ project.duration }}</span>
-            <span class="content-header">Tasks</span>
-            <ul>
+            <span class="list-element list-detail">
+                <b>Technologies</b>: {{ project.technologies }}
+            </span>
+            <span class="list-element list-detail">{{ project.description }}</span>
+            <span class="content-header" v-if="project.tasks.length !=0">Tasks</span>
+            <ul v-if="project.tasks.length !=0">
                 <li v-for="task in project.tasks" :key="task.id">{{ task.name }}</li>
             </ul>
         </div>
@@ -21,23 +24,27 @@ export default {
             projects: [
                 {
                     id: 1,
-                    name: 'Project One',
-                    description: 'This is a description of Project One',
-                    duration: 'January',
-                    tasks: [
-                        { id: 1, name: 'Task One' },
-                        { id: 2, name: 'Task Two' },
-                    ],
+                    name: 'DigiMarket',
+                    description: 'DigiMarket is a personal product, a software for management of restaurants and lounges. It provides for centralised management of multiple sites. It integrates creation and editing of orders and receipts as well as financial statistics and stock management.',
+                    duration: 'August - September 2022',
+                    technologies: 'Laravel, ReactJs, MySQL, PHP Swagger',
+                    tasks: [],
                 },
                 {
                     id: 2,
-                    name: 'Project Two',
-                    description: 'This is a description of Project Two',
-                    duration: 'February',
-                    tasks: [
-                        { id: 3, name: 'Task Three' },
-                        { id: 4, name: 'Task Four' },
-                    ],
+                    name: 'Cloud Computing Project',
+                    description: 'Conception and implementation of a monitoring tool for virtual machines managed by qemu to detect depth of clusters in qcow2 disks and the frequency of read-write operations in these disks.',
+                    duration: 'November 2020 - January 2021',
+                    technologies: 'Linux qemu, C, Python',
+                    tasks: [],
+                },
+                {
+                    id: 3,
+                    name: 'Communication interface between Clinical Equipment and SIGELAM',
+                    description: 'Conception and implementation of a means of information exchange between clinical equipment and SIGELAM. The project was aimed at connecting analysers to the information system of the laboratory to provide for result forwarding from the analyser to the information system of the laboratory.',
+                    duration: 'August 2020 - September 2020',
+                    technologies: 'Python-FTP, Python-astm, Python-xml, Postgresql',
+                    tasks: [],
                 },
             ],
         };
--- src/components/sections/Publications.vue
+++ src/components/sections/Publications.vue
@@ -7,6 +7,9 @@
                 <span class="list-element list-detail">{{ publication.authors }}</span>
                 <span class="list-element list-detail">{{ publication.journal }}</span>
                 <span class="list-element list-date">{{ publication.year }}</span>
+                <span class="list-element list-date">
+                    <a :href="publication.href" target="_blank" class="list-date">View Publication</a>
+                </span>
             </li>
         </ul>
     </div>
@@ -23,6 +26,7 @@ export default {
                     title: 'TGVM: Live Migration of Virtual Machines on Heterogenous Same ISA Processors',
                     authors: 'Caleb FONYUY-ASHERI, Djob Mvondo, Fred FABO ONANINA, Alain TCHANA',
                     journal: 'SPMA 2022',
+                    href: 'https://drive.google.com/file/d/1eXi8kBIA19SA-d2WMRATj6IK9nR17rhj/view',
                     year: 2022
                 }
             ]
